8f5c7d50c289f326c934400665f69babd3e6c090
Jun Trinh <dungtrinh1993@gmail.com>
2017-02-11 23:54:23 -0800

    add the fragment for tab view

---
+++ app/src/main/java/com/tinkersstudio/musiccloud/FragmentUserInfo.java
@@ -0,0 +1,20 @@
+package com.tinkersstudio.musiccloud;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by juntrinh on 2/11/17.
+ */
+
+public class FragmentUserInfo extends Fragment{
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_user_info, container, false);
+        return rootView;
+    }
+}
