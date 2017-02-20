cd8f6e7b382b02ca3bc72d814739b2a03c7d4421
Dung (Jun) Trinh <dungtrinh1993@gmail.com>
2017-02-19 21:12:49 -0800

    add the case for activity and in menu

--- app/src/main/java/com/tinkersstudio/musiccloud/MainActivity.java
+++ app/src/main/java/com/tinkersstudio/musiccloud/MainActivity.java
@@ -38,7 +38,7 @@ import es.dmoral.toasty.Toasty;
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
-    private static final String LOG_TAG = "MainScreen";
+    private static final String LOG_TAG = "MainActivity";
 
     /* Intent use for binding with service */
     private Intent bindingIntent;
@@ -102,32 +102,7 @@ public class MainActivity extends AppCompatActivity
             super.onBackPressed();
         }
     }
-
-    /**
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        //getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-
-     **/
-
+    
     @Override
     public boolean onNavigationItemSelected(MenuItem menuItem) {
         int id = menuItem.getItemId();
