0e60c77ce9edcd29792d311932ceb17c280a4837
Dung (Jun) Trinh <dungtrinh1993@gmail.com>
2017-04-27 04:12:31 -0700

    add the core for the database

---
+++ app/src/main/java/com/tinkersstudio/musiccloud/util/database/DatabaseHelper.java
@@ -0,0 +1,71 @@
+package com.tinkersstudio.musiccloud.util.database;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+/**
+ * Created by Owner on 4/27/2017.
+ */
+
+public class DatabaseHelper extends SQLiteOpenHelper {
+
+    /** The name of the database. */
+    public static final String DATABASE_NAME = "musiccloud.songdb";
+
+    /** The starting database version. */
+    public static final int DATABASE_VERSION = 1;
+
+    /**
+     * Create a helper object to create, open, and/or manage a database.
+     * This method always returns very quickly.  The database is not actually
+     * created or opened until one of {@link #getWritableDatabase} or
+     * {@link #getReadableDatabase} is called.
+     *
+     * @param context to use to open or create the database
+     * @param name    of the database file, or null for an in-memory database
+     * @param factory to use for creating cursor objects, or null for the default
+     * @param version number of the database (starting at 1); if the database is older,
+     *                {@link #onUpgrade} will be used to upgrade the database; if the database is
+     *                newer, {@link #onDowngrade} will be used to downgrade the database
+     */
+    public DatabaseHelper(Context context, String name, SQLiteDatabase.CursorFactory factory, int version) {
+        super(context, name, factory, version);
+    }
+
+    /**
+     * Called when the database is created for the first time. This is where the
+     * creation of tables and the initial population of the tables should happen.
+     *
+     * @param db The database.
+     */
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+
+    }
+
+    /**
+     * Called when the database needs to be upgraded. The implementation
+     * should use this method to drop tables, add tables, or do anything else it
+     * needs to upgrade to the new schema version.
+     * <p>
+     * <p>
+     * The SQLite ALTER TABLE documentation can be found
+     * <a href="http://sqlite.org/lang_altertable.html">here</a>. If you add new columns
+     * you can use ALTER TABLE to insert them into a live table. If you rename or remove columns
+     * you can use ALTER TABLE to rename the old table, then create the new table and then
+     * populate the new table with the contents of the old table.
+     * </p><p>
+     * This method executes within a transaction.  If an exception is thrown, all changes
+     * will automatically be rolled back.
+     * </p>
+     *
+     * @param db         The database.
+     * @param oldVersion The old database version.
+     * @param newVersion The new database version.
+     */
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+}
---
+++ app/src/main/java/com/tinkersstudio/musiccloud/util/database/SongCursorAdapter.java
@@ -0,0 +1,54 @@
+package com.tinkersstudio.musiccloud.util.database;
+
+import android.content.Context;
+import android.database.Cursor;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.CursorAdapter;
+
+/**
+ * Created by Owner on 4/27/2017.
+ */
+
+public class SongCursorAdapter extends CursorAdapter {
+    /**
+     * Constructor that allows control over auto-requery.  It is recommended
+     * you not use this, but instead {@link #CursorAdapter(Context, Cursor, int)}.
+     * When using this constructor, {@link #FLAG_REGISTER_CONTENT_OBSERVER}
+     * will always be set.
+     *
+     * @param context     The context
+     * @param c           The cursor from which to get the data.
+     * @param autoRequery If true the adapter will call requery() on the
+     *                    cursor whenever it changes so the most recent
+     */
+    public SongCursorAdapter(Context context, Cursor c, boolean autoRequery) {
+        super(context, c, autoRequery);
+    }
+
+    /**
+     * Makes a new view to hold the data pointed to by cursor.
+     *
+     * @param context Interface to application's global information
+     * @param cursor  The cursor from which to get the data. The cursor is already
+     *                moved to the correct position.
+     * @param parent  The parent to which the new view is attached to
+     * @return the newly created view.
+     */
+    @Override
+    public View newView(Context context, Cursor cursor, ViewGroup parent) {
+        return null;
+    }
+
+    /**
+     * Bind an existing view to the data pointed to by cursor
+     *
+     * @param view    Existing view, returned earlier by newView
+     * @param context Interface to application's global information
+     * @param cursor  The cursor from which to get the data. The cursor is already
+     */
+    @Override
+    public void bindView(View view, Context context, Cursor cursor) {
+
+    }
+}
